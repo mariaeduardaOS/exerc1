5780d66ba8dee708f58ea93bd193ac1c3c56fb1b
mariaeduardaOS <83980857+mariaeduardaOS@users.noreply.github.com>
2024-08-20 08:58:25 -0300

    Update autor.java

--- autor.java
+++ autor.java
@@ -2,13 +2,34 @@ public class Autor {
     private int idAutor;
     private String nome;
     private String nacionalidade;
-    // ... getters e setters
-}
 
-public class Livro {
-    private int idLivro;
-    private String titulo;
-    private int anoPublicacao;
-    private Autor autor; // Associação com a classe Autor
-    // ... getters e setters
+ public Autor(int idAutor, String nome, String nacionalidade) {
+        this.idAutor = idAutor;
+        this.nome = nome;
+        this.nacionalidade = nacionalidade;
+    }
+
+    public int getIdAutor() {
+        return idAutor;
+    }
+
+    public void setIdAutor(int idAutor) {
+        this.idAutor = idAutor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
 }
